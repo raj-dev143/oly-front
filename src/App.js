a72c90ef7c37d1c420dbbdb153e18b67fd8af010
raj-dev143 <rajendra.meena.dev@gmail.com>
2024-05-22 19:02:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
             }
           />
           <Route
-            path="/rest"
+            path="/rest/"
             element={
               <PrivateRoute>
                 <RestaurantRegister />
